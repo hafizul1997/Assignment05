3ad5571a23394904426e6829fbac17d4eb468062
Hafizul Islam <hafizulislam1647381997@gmail.com>
2026-09-11 22:07:40 +0400

    Initialize React-Tyscript Project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <>
+    {/*Navbar*/}
+     {/*Hero Section*/}
+      {/* Product list*/}
+     {/*Add to Cart*/}
+      {/*Footer */}
+    </>
+  )
+}
+
+export default App
