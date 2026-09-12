d3fee966c497c763633d5bbdef570fbff629174a
Hafizul Islam <hafizulislam1647381997@gmail.com>
2026-09-12 17:57:16 +0400

    Updated in Hero Section Explore Technologies and Learn More Button using State

--- src/Components/Hero.tsx
+++ src/Components/Hero.tsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import HeroImage from '../assets/banner-stack.png'
 const Hero = () => {
+    const [herobtn,setHeroBtn]=useState('Explore Technologies');
+    const handleHeroBtn=(type:"Explore Technologies"|"Learn More")=>{
+        setHeroBtn(type);
+    }
     return (
         <section>  
             <div className='max-w-[1280px] mx-auto' >  
@@ -12,8 +16,8 @@ const Hero = () => {
 compare them side by side, and put together the stack that fits your
 next project.</p>
 <div className='flex gap-4 mt-7'>
-    <button className='bg-gradient-to-r from-[#f97316] via-[#f0556f] to-[#ec4899] text-white font-bold px-6 py-3 rounded-2xl'>Explore Technologies</button>
-    <button>Learn More</button>
+    <button onClick={()=>handleHeroBtn("Explore Technologies")} className={`btn px-14 ${herobtn ==="Explore Technologies" ? "text-white bg-gradient-to-r from-[#f97316] via-[#f0556f] to-[#ec4899]" : ""} py-7 rounded-2xl text-bold`}>Explore Technologies</button>
+    <button onClick={()=>handleHeroBtn("Learn More")} className={`btn px-22 ${herobtn ==="Learn More" ? "text-white bg-gradient-to-r from-[#f97316] via-[#f0556f] to-[#ec4899]" : ""} py-7 rounded-2xl text-bold`}>Learn More</button>
 </div>
             </div>
             <img src={HeroImage} alt="Banner Stack"/>
